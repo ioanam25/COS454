1d834aeb0c3d8a51338bbd3fad634dcdb9223882
Admin <ioanam@princeton.edu>
2022-12-05 10:49:18 -0500

    some training stuff

--- main.py
+++ main.py
@@ -1,16 +1,118 @@
-# This is a sample Python script.
+import torch
+import torch.nn as nn
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # N: number of examples in batch
+        # C: number of channels
+        # H: image height in pixels
+        # W: image width in pixels
+        K_c = 4 # K_c: number of colors
+        K_s = 4 # K_s: number of shapes
+        # Let x be image batch: tensor of shape [N, C, H, W]
+        # Let encoder be an encoding function with final nonlinearity
+        # input must be 224 x 224 for ResNet-18
+        self.encoder = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+        # Set up network layers
+        self.D_pre = 1000 # output of resnet
+        self.color_enc = nn.Linear(self.D_pre, K_c)
+        self.shape_enc = nn.Linear(self.D_pre, K_s)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    # Forward pass
+    def forward(self, x):
+        z_pre = self.encoder(x)  # shape: [N, D_pre]
+        z_c = self.color_enc(z_pre)  # shape: [N, K_c]
+        z_s = self.shape_enc(z_pre)  # shape: [N, K_s]
+        z_cs = torch.reshape(torch.unsqueeze(z_c, -1) + torch.unsqueeze(z_s, 1), (N, -1))  # shape: [N, K_c * K_s]
+        return z_cs
 
+class Train():
+    def train_one_epoch(epoch_index, tb_writer, training_loader, loss_fn, optimizer, model):
+        running_loss = 0.
+        last_loss = 0.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        # Here, we use enumerate(training_loader) instead of
+        # iter(training_loader) so that we can track the batch
+        # index and do some intra-epoch reporting
+        for i, data in enumerate(training_loader):
+            # Every data instance is an input + label pair
+            inputs, labels = data
 
+            # Zero your gradients for every batch!
+            optimizer.zero_grad()
 
-# Press the green button in the gutter to run the script.
+            # Make predictions for this batch
+            outputs = model(inputs)
+
+            # Compute the loss and its gradients
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+
+            # Adjust learning weights
+            optimizer.step()
+
+            # Gather data and report
+            running_loss += loss.item()
+            if i % 1000 == 999:
+                last_loss = running_loss / 1000  # loss per batch
+                print('  batch {} loss: {}'.format(i + 1, last_loss))
+                tb_x = epoch_index * len(training_loader) + i + 1
+                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+                running_loss = 0.
+
+        return last_loss
+
+    def training_loop(self, training_set):
+        # Initializing in a separate cell so we can easily add more epochs to the same run
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+        epoch_number = 0
+
+        EPOCHS = 5
+        loss_fn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        model = Net()
+
+        best_vloss = 1_000_000
+
+        training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True, num_workers=2)
+
+        for epoch in range(EPOCHS):
+            print('EPOCH {}:'.format(epoch_number + 1))
+
+            # Make sure gradient tracking is on, and do a pass over the data
+            model.train(True)
+            avg_loss = train_one_epoch(epoch_number, writer)
+
+            # We don't need gradients on to do reporting
+            model.train(False)
+
+            running_vloss = 0.0
+            for i, vdata in enumerate(validation_loader):
+                vinputs, vlabels = vdata
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss
+
+            avg_vloss = running_vloss / (i + 1)
+            print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+            # Log the running loss averaged per batch
+            # for both training and validation
+            writer.add_scalars('Training vs. Validation Loss',
+                               {'Training': avg_loss, 'Validation': avg_vloss},
+                               epoch_number + 1)
+            writer.flush()
+
+            # Track best performance, and save the model's state
+            if avg_vloss < best_vloss:
+                best_vloss = avg_vloss
+                model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+                torch.save(model.state_dict(), model_path)
+
+            epoch_number += 1
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
