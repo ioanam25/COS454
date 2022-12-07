d211dae28909ecafa41ecc03568883ba842738a6
Admin <ioanam@princeton.edu>
2022-12-07 14:55:57 -0500

    dataloader

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from PIL import Image
 import torchvision.transforms as transforms
 from pip._internal.utils import datetime
 from torch.utils.tensorboard import SummaryWriter
+import numpy as np
 import os
 import pandas as pd
+from torch.utils.data import Dataset, DataLoader
 
 class Preprocessing():
     def get_image_tensors_array(self):
@@ -27,7 +29,8 @@ class Preprocessing():
 
         return X, y
 
-    def train_test_split(self, X, y):
+    def train_test_split(self):
+        X, y = self.get_image_tensors_array()
         train_idx = []
         test_idx = []
         for index, image in enumerate(X):
@@ -36,7 +39,43 @@ class Preprocessing():
             else:
                 train_idx.append(index)
 
+        X_train = []
+        y_train = []
+        X_test = []
+        y_test = []
+        for i in range(train_idx):
+            X_train.append(X[train_idx[i]])
+            y_train.append(y[train_idx[i]])
 
+        for i in range(test_idx):
+            X_test.append(X[test_idx[i]])
+            y_test.append(y[test_idx[i]])
+
+        return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
+
+class CustomDataset(Dataset):
+    def __init__(self, inputs, labels):
+        self.inputs = inputs
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        input = self.input[idx]
+        label = self.labels[idx]
+        sample = {"Image": input, "Labels": label}
+        return sample
+
+
+dataset = Preprocessing.train_test_split()
+training_set = dataset[0], dataset[1]
+test_set = dataset[2], dataset[3]
+image_labels_df_train = pd.DataFrame({'Image': training_set[0], 'Labels': training_set[1]})
+image_labels_df_test = pd.DataFrame({'Image': test_set[0], 'Labels': test_set[1]})
+
+# define data set object
+training_data = CustomDataset(image_labels_df_train['Image'], image_labels_df_train['Labels'])
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -67,35 +106,6 @@ class Net(nn.Module):
 model = Net()
 
 
-def train_one_epoch(epoch_index, tb_writer, training_loader, loss_fn, optimizer):
-    running_loss = 0.
-    last_loss = 0.
-
-    # Here, we use enumerate(training_loader) instead of
-    # iter(training_loader) so that we can track the batch
-    # index and do some intra-epoch reporting
-    for i, data in enumerate(training_loader):
-        # Every data instance is an input + label pair
-        inputs, labels = data
-
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # Gather data and report
-        running_loss += loss.item()
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000  # loss per batch
-            print('  batch {} loss: {}'.format(i + 1, last_loss))
-            tb_x = epoch_index * len(training_loader) + i + 1
-            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-            running_loss = 0.
-
-    return last_loss
-
-
 class Train():
     def training_loop(self, training_set):
         # Initializing in a separate cell so we can easily add more epochs to the same run
@@ -103,19 +113,35 @@ class Train():
         writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
         epoch_number = 0
 
-        EPOCHS = 5
+        # TODO EPOCHS = ???
         loss_fn = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-        best_vloss = 1_000_000
-
-        training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True, num_workers=2)
+        training_loader = torch.utils.data.DataLoader(training_data, batch_size=20, shuffle=True, num_workers=2) # TODO choose batch size
 
         for epoch in range(EPOCHS):
             print('EPOCH {}:'.format(epoch_number + 1))
-            model.train(True)
-            avg_loss = train_one_epoch(self, epoch_number, writer, training_loader, loss_fn, optimizer)
-            epoch_number += 1
+            running_loss = 0.
+
+            for i, data in enumerate(training_loader):
+                # Every data instance is an input + label pair
+                inputs, labels = data
+
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = loss_fn(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # Gather data and report
+                running_loss += loss.item()
+                if i % 1000 == 999:
+                    last_loss = running_loss / 1000  # loss per batch TODO check??
+                    print('  batch {} loss: {}'.format(i + 1, last_loss))
+                    x = epoch * len(training_set) + i + 1
+                    writer.add_scalar('Loss/train', last_loss, x)
+                    running_loss = 0.
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
